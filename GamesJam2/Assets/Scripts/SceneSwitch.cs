96b5990391509d48c666e414ec3ef35d5455f087
Tobias Fox <tobiasfox@gmx.net>
2019-03-21 15:10:12 +0100

    add simple intro scene

---
+++ GamesJam2/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private AsyncOperation asyncOperation;
+
+    private void Start()
+    {
+        StartAsyncSceneLoading("TobiasScene");
+    }
+
+    public void StartAsyncSceneLoading(string sceneName)
+    {
+        StartCoroutine(LoadScene(sceneName));
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        asyncOperation.allowSceneActivation = false;
+
+        yield return null;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            asyncOperation.allowSceneActivation = true;
+        }
+    }
+
+}
